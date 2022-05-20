12299b2ac1d87996a750012a169172efa37420c9
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-21 00:19:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -15,25 +15,6 @@ logging.basicConfig(
 
 LOGGER = logging
 
-def get_config_from_url():
-    CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL', None)
-    try:
-        if len(CONFIG_FILE_URL) == 0: raise TypeError
-        try:
-            res = requests.get(CONFIG_FILE_URL)
-            if res.status_code == 200:
-                logging.info("Config uzaktan alındı. Status 200.")
-                with open('config.env', 'wb+') as f:
-                    f.write(res.content)
-                    f.close()
-            else:
-                logging.error(f"Failed to download config.env {res.status_code}")
-        except Exception as e:
-            logging.error(f"CONFIG_FILE_URL: {e}")
-    except TypeError:
-        pass
-
-get_config_from_url()
 if os.path.exists('config.env'):
     load_dotenv('config.env')
 
