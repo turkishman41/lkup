cedf3001d6463f043fab6f0fdf24fd31ec0d30a7
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-29 13:26:05 +0300

    Update ytdlp_trigger.py

--- plugins/ytdlp_trigger.py
+++ plugins/ytdlp_trigger.py
@@ -183,9 +183,9 @@ async def echo(bot, update):
                 duration = current_r_json["duration"]
             if "formats" in current_r_json:
                 format_ext = formats.get("ext")
-                    get_data = await db.get_blocked_exts(update.from_user.id)
-                    if format_ext not in get_data:
-                        continue
+                get_data = await db.get_blocked_exts(update.from_user.id)
+                if format_ext not in get_data:
+                    continue
                 for formats in current_r_json["formats"]:
                     format_id = formats.get("format_id")
                     format_string = formats.get("format_note")
