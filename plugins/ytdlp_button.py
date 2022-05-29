618db10ed7cc7370e97d637d4fad2701cd713b84
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-29 16:38:52 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -204,6 +204,9 @@ async def yt_dlp_call_back(bot, update):
     if ".cloud" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://vidmoly.to/")
+    if "cdn2.orospucocugi.xyz" in yt_dlp_url:
+        command_to_exec.append("--referer")
+        command_to_exec.append("https://www.canlitv.news/")
     if "closeload" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://closeload.com/")
