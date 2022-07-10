2af94d7e8625669a7b1cf7724d583984b1413652
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-07-11 01:31:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 
 # get a token from @BotFather
-STRING_SESSION = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # The Telegram API things
 # Get these values from my.telegram.org
