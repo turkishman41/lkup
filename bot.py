6c32d19bddf45bc6e24360a9bfd4f52f5840fa78
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2022-07-14 11:53:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@
 import logging
 import os
 import time
-from config import BOT_TOKEN, APP_ID, API_HASH, DOWNLOAD_LOCATION, OWNER_ID, SESSION_NAME, SEND_LOGS_WHEN_DYING, STRING_SESSION
+from config import BOT_TOKEN, APP_ID, API_HASH, DOWNLOAD_LOCATION, OWNER_ID, SESSION_NAME, SEND_LOGS_WHEN_DYING, STRING_SESSION, bot, userbot
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from functions.utils import ReadableTime
@@ -14,12 +14,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 botStartTime = time.time()
-
-
-
-bot = Client(name=SESSION_NAME, api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, workers=343, plugins={"root": "plugins"}, sleep_threshold=5)
-
-userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION)
 
 app = bot()
 app.run()
