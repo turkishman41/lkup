d30e50fa4849d051a18e508449f7554f39350a27
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2022-07-14 23:50:56 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,7 +33,7 @@ async def start_handler(c: Client, m: "types.Message"):
         forcsub = await client.create_chat_invite_link(PRE_LOG, member_limit=1)
     except Exception as e:
         Logger.error(e)
-           return
+        return
     try:
         await client.send_message(
             chat_id=message.from_user.id,
