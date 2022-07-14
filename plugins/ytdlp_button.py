fc134ed9e679b9cf813a5ad5fbc954fa064719f1
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2022-07-14 13:28:44 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -5,7 +5,9 @@ import shutil
 import time
 from datetime import datetime
 
-from config import DOWNLOAD_LOCATION, LOG_CHANNEL, HTTP_PROXY, TG_MAX_FILE_SIZE, DEF_WATER_MARK_FILE, PROMO, PRE_LOG, app, ubot
+from config import DOWNLOAD_LOCATION, LOG_CHANNEL, HTTP_PROXY, TG_MAX_FILE_SIZE, DEF_WATER_MARK_FILE, PROMO, PRE_LOG
+
+from bot import app, ubot
 from pyrogram.enums import MessageEntityType, ChatAction
 from database.database import db
 from translation import Translation
