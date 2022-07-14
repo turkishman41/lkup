122c8a21796985debbe88caae8589f0f2c9da609
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2022-07-14 16:15:45 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -407,6 +407,13 @@ async def yt_dlp_call_back(bot, update):
                                 start_time
                             )
                         )
+                    if LOG_CHANNEL:
+                        await bot.copy_message(chat_id)
+                except FloodWait as e:
+                    print(f"Sleep of {e.value} required by FloodWait ...")
+                    time.sleep(e.value)
+                except MessageNotModified:
+                    pass
 
                 end_two = datetime.now()
                 time_taken_for_upload = (end_two - end_one).seconds
