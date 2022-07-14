b9cf14d71c2a3c201e1748c455e3f0227f3ced4b
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2022-07-14 14:57:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -28,11 +28,11 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 STRING_SESSION = environ.get("STRING_SESSION", "")
 if len(STRING_SESSION) == 0:
         raise KeyError
-userbot = Client(name='userbot', api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
-    if not userbot:
-        LOGGER.error("Cannot initialized User Session. Please regenerate STRING_SESSION")
-    else:
-        userbot.start()
+userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
+     if not userbot:
+         LOGGER.error("Cannot initialized User Session. Please regenerate STRING_SESSION")
+     else:
+         userbot.start()
 
 # Where files larger than 2 GB will go
 PRE_LOG = environ.get("PRE_LOG", "") 
