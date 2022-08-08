cb3f1f9882e7c8483e8270db7573c3d53bf7d818
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2022-08-08 14:39:51 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -433,8 +433,8 @@ async def yt_dlp_call_back(bot, update):
                             bot.copy_message(chat_id=self.__user_id, from_chat_id=self.__sent_msg.chat.id, message_id=self.__sent_msg.id)
                         except Exception as err:
                             await bot.send_message(OWNER_ID, f"Failed To Send Video in PM:\n{err}")
-                if LOG_CHANNEL:
-                    await copy.copy(chat_id)
+                    if LOG_CHANNEL:
+                        await copy.copy(chat_id)
                 except FloodWait as e:
                     print(f"Sleep of {e.value} required by FloodWait ...")
                     time.sleep(e.value)
