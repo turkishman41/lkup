b04b4539bdf2db272258922ece0a3b45955b2ecb
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2022-08-08 15:23:52 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -31,27 +31,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
-class Urlyukle:
-    def __init__(self, name=None, listener=None):
-        self.name = name
-        self.uploaded_bytes = 0
-        self._last_uploaded = 0
-        self.__listener = listener
-        self.__start_time = time()
-        self.__total_files = 0
-        self.__is_cancelled = False
-        self.__as_doc = AS_DOCUMENT
-        self.__thumb = f"Thumbnails/{listener.message.from_user.id}.jpg"
-        self.__msgs_dict = {}
-        self.__corrupted = 0
-        self.__resource_lock = RLock()
-        self.__is_corrupted = False
-        self.__sent_msg = bot.get_messages(self.__listener.message.chat.id, self.__listener.uid)
-        self.__user_settings()
-        self.__leech_log = PRE_LOG.copy()  # copy then pop to keep the original var as it is
-        self.__bot = bot
-        self.__user_id = listener.message.from_user.id
-        self.isPrivate = listener.message.chat.type in ['private', 'group'] 
+ 
 
 async def yt_dlp_call_back(bot, update):
     cb_data = update.data
@@ -428,13 +408,14 @@ async def yt_dlp_call_back(bot, update):
                                 start_time
                             )
                        ) 
-                    if BOT_PM:
+                if BOT_PM:
+                    try:
                         await bot.copy_messages(
                                 chat_id=chat_id, 
                                 from_chat_id=PRE_LOG, 
                                 message_id=copy.id)
-                except Exception as f:
-                    bot.send_message(OWNER_ID, f"{f}") 
+                    except Exception as f:
+                        bot.send_message(OWNER_ID, f"{f}") 
                     if LOG_CHANNEL:
                         await copy.copy(LOG_CHANNEL)
                 except FloodWait as e:
