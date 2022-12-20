0e33f5fa28cd6f46da3f17926050f138daba2e20
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2022-12-20 12:11:00 +0300

    Create time.py

---
+++ plugins/time.py
@@ -0,0 +1,15 @@
+# written by :d
+import pyrogram
+import time
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from functions.utils import ReadableTime
+from pyromod import listenimport logging
+import os
+import time
+
+botStartTime = time.time()
+
+Client.on_message(filters.private & filters.command(["yas", "time"]))
+async def time(c: Client, m: "types.Message"):
+    m.reply_text(f"yaşım: {ReadableTime(time.time() - botStartTime)}"
