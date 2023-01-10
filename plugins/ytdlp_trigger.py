8d5c1626bd5b2690b005fe0c524b89af8cf7d7b9
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-10 10:15:04 +0300

    Update ytdlp_trigger.py

--- plugins/ytdlp_trigger.py
+++ plugins/ytdlp_trigger.py
@@ -119,6 +119,9 @@ async def echo(bot, update):
     if ".cloud" in url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://vidmoly.to/")
+    if ".space" in url:
+        command_to_exec.append("--referer")
+        command_to_exec.append("https://vidmoly.to/")
     if "https://upstreamcdn.co" in url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://upstreamcdn.co")
