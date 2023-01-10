01eec8e7aead9741d4e426875ce1635bf22d2d2e
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-10 11:15:11 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -212,6 +212,9 @@ async def yt_dlp_call_back(bot, update):
     if ".cloud" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://vidmoly.to/")
+    if ".sibnet.ru" in yt_dlp_url:
+        command_to_exec.append("--referer")
+        command_to_exec.append("https://sibnet.ru/")
     if ".space" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://vidmoly.to/")
