c261d2f34e31737810f0fca2ec7082c033876e0e
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-02-21 21:12:13 +0300

    Update ytdlp_trigger.py

--- plugins/ytdlp_trigger.py
+++ plugins/ytdlp_trigger.py
@@ -154,7 +154,6 @@ async def echo(bot, update):
             "yt-dlp",
             "--no-warnings",
             "--external-downloader","aria2c", 
-            "--allow-unplayable-formats", 
             "--youtube-skip-dash-manifest",
             "--no-check-certificate",
             "-j",
